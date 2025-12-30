dd954bd8f3e6bad35ba2bc0fab5524864430c8c8
Tabish <mdtabishfiroz04@gmail.com>
2025-12-30 17:35:06 +0530

    Array

---
+++ 8-Array/script.js
@@ -0,0 +1,34 @@
+let fruits = ["Apple", "Banana", "Arange"]
+// for (let i = 0; i < fruits.length; i++)
+// {
+//     console.log(fruits[i])
+// }
+
+// for (let fruit of fruits)
+// {
+//     console.log(fruit)
+// }
+fruits.sort().reverse()
+for (let i = fruits.length - 1; i >= 0; i--)
+{
+    console.log(fruits[i])
+}
+
+// fruits.push("Coconut")
+// fruits.pop()
+// fruits.unshift("Mango")
+// fruits.shift()
+// console.log(fruits[2])
+
+// let numOfFruits = fruits.length;
+// console.log(numOfFruits)
+
+// let index = fruits.indexOf("Banana")
+// console.log(index)
+
+
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[3])
+// console.log(fruits[4])
