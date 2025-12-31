36ad6c594271d2b969bfc43d36d6188806f437b6
firozmdtabish04 <mdtabish04@gmail.com>
2025-12-31 23:34:29 +0530

    ...rest

---
+++ 10-Rest_Parameter/script.js
@@ -0,0 +1,45 @@
+// function openFridge(...foods)
+// {
+//     console.log(foods)
+// }
+// const food1 = "Pizza"
+// const food2 = "IceCream"
+// const food3 = "Burger"
+
+// openFridge(food1,food2,food3)
+// function getFood(...foods)
+// {
+//     console.log(foods)
+// }
+// const foods = getFood(food1,food2, food3)
+
+// function sum(...numbers)
+// {
+//     let result = 0
+//     for (let number of  numbers)
+//     {
+//         result += number;
+//     }
+//     return result
+// }
+// const total = sum(1)
+// console.log(`your total is $${total}`)
+
+// function getAverage(...numbers)
+// {
+//     let result = 0
+//     for (let number of numbers)
+//     {
+//         result += number
+//     }
+//     return result / numbers.length
+// }
+// const total = getAverage(3, 2,3,4)
+// console.log(total)
+
+// function combineStrings(...strings)
+// {
+//     return strings.join(" ")
+// }
+// const fullName = combineStrings("Tabish", "Firoz")
+// console.log(fullName)
