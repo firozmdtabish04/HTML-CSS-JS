80d6d37b2f469bdb37183021444b789fd3beaca0
firozmdtabish04 <mdtabish04@gmail.com>
2026-01-06 14:17:57 +0530

    Map

---
+++ 13-Map/script.js
@@ -0,0 +1,43 @@
+// Create a new Map
+// const fruits = new Map();
+// Set Map Values
+// fruits.set("apples", 500);
+// fruits.set("bananas", 300);
+// fruits.set("oranges", 200);
+
+// let numb = fruits.get("apples");
+
+// document.getElementById("demo").innerHTML = "There are " + numb + " apples.";
+
+
+// const numbers = [1, 2, 3, 4, 5];
+// const squares = numbers.map(square);
+// const cubes = numbers.map(cube);
+
+// console.log(cubes);
+
+// function square(element) {
+//   return Math.pow(element, 2);
+// }
+// function cube(element) {
+//   return Math.pow(element, 3);
+// }
+
+
+// const students = ["Tabish", "Abhilipsa", "Abhi", "Saitaan"];
+// const studentUpper = students.map(upperCase);
+// const studentLower = students.map(lowerCase);
+
+// console.log(studentUpper);
+// console.log(studentLower);
+
+// function upperCase(element)
+// {
+//     return element.toUpperCase();
+// }
+
+// function lowerCase(element) {
+//   return element.toLowerCase();
+// }
+
+ 
