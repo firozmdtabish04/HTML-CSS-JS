c6ca4dc087ed5cd3b5823af4877a779193f764c2
firozmdtabish04 <mdtabish04@gmail.com>
2026-01-06 13:32:24 +0530

    For Each

---
+++ 12-ForEach/script.js
@@ -0,0 +1,56 @@
+// let numbers = [1, 2, 3, 4, 5]
+
+// numbers.forEach(cube)
+// numbers.forEach(square)
+// numbers.forEach(triple)
+// numbers.forEach(double)
+// numbers.forEach(display)
+
+// function double(element, index, array) {
+//   array[index] = element * 2;
+// }
+// function triple(element, index, array) {
+//   array[index] = element * 3;
+// }
+
+// function square(element, index, array)
+// {
+//     array[index] = Math.pow(element, 2)
+// }
+// function cube(element, index, array)
+// {
+//     array[index] = Math.pow(element, 3)
+// }
+
+// function display(element)
+// {
+//     console.log(element)
+// }
+
+
+
+// Function to display each element
+let fruits = ["Apple", "Orange", "Banana", "Coconut"];
+fruits.forEach(capitalize);
+// fruits.forEach(lowerCase);
+// fruits.forEach(uppercase);
+fruits.forEach(display);
+
+function uppercase(element, index, array)
+{
+    array[index] = element.toUpperCase();
+}
+function lowerCase(element, index, array)
+{
+    array[index] = element.toLowerCase();
+}
+
+function capitalize(element, index, array)
+{
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+}
+
+function display(element)
+{
+    console.log(element);
+}
